ca6f9fc507030a129ef2da5e1eb4f45295a290b7
Ryan-Vacek <rvacek@ucsc.edu>
2014-05-15 07:21:12 -0700

    -Created main menu

---
+++ BSUMars-UnityProject/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public GameObject main;
+	public GameObject credits;
+	public GameObject back;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnClick() {
+		if (name == "Credit") {
+			NGUITools.SetActive(main, false);
+			NGUITools.SetActive(credits, true);
+		} else if (name == "Play") {
+			Application.LoadLevel("mars_template");
+		} else if (name == "Back") {
+			NGUITools.SetActive(credits, false);
+			NGUITools.SetActive(main, true);
+		}
+	}
+}
